072bc51ed3dad382829cecc0851a04cfa83aa997
sshivahh <russelshivah16@gmail.com>
2023-10-11 14:02:28 +0700

    Import foto makanan, bikin tampilan kasar, styling dikit

--- Pages/home.php
+++ Pages/home.php
@@ -1,7 +1,11 @@
 <?php
 
     require_once('./../Database/db.php');
+    $sql = "SELECT * FROM menu";
 
+    $data = $db->query($sql);
+
+    session_start();
 ?>
 
 <html>
@@ -10,6 +14,57 @@
         <link rel="stylesheet" href="./../Styles/home.css" />
     </head>
     <body>
-        <nav></nav>
+        <nav>
+            <div class="logo">
+                <h2 class="logo-blue">IF330<h2>-A6</h2></h2>
+            </div>
+            <div class="search-bar">
+                <input type="text" placeholder="Search Menu or Categories"/>
+                <button>
+                    <img src="./../Src/Icons/Search.png" alt="search" />
+                </button>
+            </div>
+            <div class='profile'>
+                <?php 
+                    if(isset($_SESSION['user'])){
+                        ?>
+                    <h2 class="profile-name"><?= $_SESSION['user']['name']?></h2>
+                    <?php
+                    }
+                    elseif(isset($_SESSION['admin'])){
+                        ?>
+                        <h2 class="profile-name"><?= $_SESSION['admin']['name']?></h2>
+                        <?php
+                    }
+                    else{
+                        ?>
+                        <a href="login.php">
+                            <button class='login-button'>Log in</button>
+                        </a>
+                    <?php
+                    }
+                    ?>
+            </div>
+        </nav>
+            <table class="menu-table">
+                <tr>
+                    <th>Name</th>
+                    <th>Picture</th>
+                    <th>Price</th>
+                    <th>Category</th>
+                    <th>Description</th>
+                </tr>
+                <?php
+                    while($row = $data->fetch(PDO::FETCH_ASSOC)){
+                ?>
+                    <tr>
+                        <td><?= $row['nama']?></td>
+                        <td><img src="./../Src/Makanan/<?= $row['gambar']?>" alt="gambar pizza" style="width: 100px"></td>
+                        <td><?= $row['harga']?></td>
+                        <td><?= $row['kategori']?></td>
+                        <td><?= $row['deskripsi']?></td>
+                    </tr>
+                <?php } ?>
+            </table>
     </body>
 </html>
---
+++ delete-me.sql
@@ -0,0 +1,11 @@
+INSERT INTO menu(nama, gambar, harga, kategori, deskripsi) VALUES
+("Margheritha Pizza", "/Pizza/Pizza_Margherita.jpg", 10, "Pizza", "Pizza with basil"),
+("Love Pizza", "/Pizza/Pizza_Love.jpg", 15, "Pizza", "Pizza in the shape of a heart"),
+("Calzone Pizza", "/Pizza/Pizza_Calzone.jpg", 20, "Pizza", "Pizza shaped like a cookie"),
+("BBQ Pizza", "/Pizza/Pizza_BBQ.jpg", 20, "Pizza", "Pizza with barbeque sauce"),
+("Hawaiian Pizza", "/Pizza/Pizza_Hawaiian.jpg", 10, "Pizza", "Pizza from Hawaii"),
+("Pepperoni Pizza", "/Pizza/Pizza_Pepperoni.jpg", 20, "Pizza", "Pizza with pepperoni"),
+("Quattro Formaggi Pizza", "/Pizza/Pizza_Quattro_Formaggi.jpg", 20, "Pizza", "Pizza with luscious cheese"),
+("Seafood Pizza", "/Pizza/Pizza_Seafood.jpg", 15, "Pizza", "Pizza with seafood toppings"),
+("Sicilian Pizza", "/Pizza/Pizza_Sicilian.jpg", 20, "Pizza", "Pizza with strong cheese");
+
