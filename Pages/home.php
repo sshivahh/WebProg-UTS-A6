4e5290073b62dc8456dcfede1bf069640e732f38
sshivahh <russelshivah16@gmail.com>
2023-10-11 11:05:15 +0700

    Tambah folder untuk taro css
    taro css di Styles, penamaan sama kaya pagenya

---
+++ Pages/home.php
@@ -0,0 +1,15 @@
+<?php
+
+    require_once('./../Database/db.php');
+
+?>
+
+<html>
+    <head>
+        <title>IF330-A6</title>
+        <link rel="stylesheet" href="./../Styles/home.css" />
+    </head>
+    <body>
+        <nav></nav>
+    </body>
+</html>
